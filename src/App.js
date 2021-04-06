9ad052f4975c4ab2d39b15ddf65ecf8e309dac13
Ajay-joshi-mtr <ajayjoshi.mtr@gmail.com>
2021-04-06 16:20:33 +0530

    tinder header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>TEST</h1>
-      {/* Header */}
+    <div className="App">     
+      <Header/>
       {/* Tinder Cards */}
       {/* Swipe Buttons */}
     </div>
